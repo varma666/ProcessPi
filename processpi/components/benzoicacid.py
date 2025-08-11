0e82ad7cf2f1ddc727703a72696dd6100ec657b3
Raviteja Varma <nravitejavarma@gmail.com>
2025-08-11 15:38:51 +0530

    Update benzoicacid.py

--- processpi/components/benzoicacid.py
+++ processpi/components/benzoicacid.py
@@ -12,8 +12,8 @@ class BenzoicAcid(Component):
     _critical_zc = 0.246  # Placeholder for critical compressibility factor
     _critical_acentric_factor = 0.6028  # Placeholder for critical acentric factor
     _density_constants = [0.71587, 0.24812, 751,0.2857]
-    _specific_heat_constants = [-5,480,647.12,0,0,0]
+    _specific_heat_constants = [-5480,647.12,0,0,0]
     _viscosity_constants = [-12.947,2557.9,0,0, 0] 
     _thermal_conductivity_constants = [0.2391, -0.0002325, 0,0,0]
-    _vapor_pressure_constants = [88.513, -11,829, -8.6826, 2.32E-19, 6] 
+    _vapor_pressure_constants = [88.513, -11829, -8.6826, 2.32E-19, 6] 
     _enthalpy_constants = [10.19E-7, 0.478, 395.45,7.1277, 0]  # Placeholder for enthalpy constants
