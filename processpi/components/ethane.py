2658bcd6cd72c2748f2c8609ba774caa10938496
Raviteja Varma <nravitejavarma@gmail.com>
2025-09-24 14:09:17 +0530

    Update ethane.py

--- processpi/components/ethane.py
+++ processpi/components/ethane.py
@@ -15,3 +15,13 @@ class Ethane(Component):
     _thermal_conductivity_constants = [0.35758, -0.0011458,6.19E-07,0,0]
     _vapor_pressure_constants = [51.857,-2598.70,-5.1283,1.49E-05, 2] 
     _enthalpy_constants = [2.1091E-7,0.60646, -0.55492,0.32799, 0]  # Placeholder for enthalpy constants
+
+    @PropertyMethod
+    def specific_heat(self) -> SpecificHeat:
+        if self._specific_heat is not None:
+            return self._specific_heat
+        T = self.temperature.value
+        Tr = T / self._critical_temperature.value
+        t = 1 - Tr
+        cp = (self._sepcific_heat_constants[0] / t ) + (self._sepcific_heat_constants[1]) - ( 2 * self._sepcific_heat_constants[0] * self._sepcific_heat_constants[2] * t) - (self._sepcific_heat_constants[0] * self._sepcific_heat_constants[3] * (t**2)) - ((self._sepcific_heat_constants[2]**2)*(t**3)/3) - ((self._sepcific_heat_constants[2] * self._sepcific_heat_constants[3] * (t**4))/2) - ((self._sepcific_heat_constants[0]**2) * (t**5)/5) 
+        return SpecificHeat(cp, "J/kgK")
