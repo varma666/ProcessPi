00b776b617cf60110fc48ad0de5d04cfd211b562
Raviteja Varma <nravitejavarma@gmail.com>
2025-09-29 13:55:40 +0530

    Update __init__.py

--- processpi/equipment/heatexchangers/mechanical/__init__.py
+++ processpi/equipment/heatexchangers/mechanical/__init__.py
@@ -1,1 +1,43 @@
+from typing import Dict, Any
+
+# Import mechanical submodules
+from . import classification
+from . import basic_params
+from . import double_pipe
+from . import shell_tube
+from . import kern_method
+from . import bell_method
+from . import condenser
+from . import reboiler
+from . import evaporator
+
+# Mapping for design_type strings to submodules
+_design_map = {
+    "Classification": classification,
+    "BasicParameters": basic_params,
+    "DoublePipe": double_pipe,
+    "ShellAndTube": shell_tube,
+    "KernMethod": kern_method,
+    "BellMethod": bell_method,
+    "Condenser": condenser,
+    "Reboiler": reboiler,
+    "Evaporator": evaporator
+}
+
+def run_mechanical_design(hx, method: str = "BellMethod", **kwargs) -> Dict[str, Any]:
+    """
+    Dispatch mechanical design calculations to the appropriate submodule.
+    
+    Default method: Bell-Delaware method
+    Optional: Kern's method
+    """
+    method = method or "BellMethod"
+
+    if method not in _design_map:
+        raise ValueError(
+            f"Unknown method '{method}'. Available options: {list(_design_map.keys())}"
+        )
+
+    module = _design_map[method]
+    return module.run(hx, **kwargs)
 
