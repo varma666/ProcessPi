20cb8105a8a39f82da6c8cde5d3097456bf7291e
Raviteja Varma <nravitejavarma@gmail.com>
2025-11-12 12:06:04 +0530

    Create examples.py

---
+++ processpi/equipment/heatexchangers/examples.py
@@ -0,0 +1,44 @@
+"""
+Example 1 
+Double Pipe Benzene-Toluene Exchanger. It is desired to heat 9820 lb/hr of cold benzene from 80 to 120°F using hot toluene which is cooled from 160 to 100°F. 
+The specific gravities at 68°F are 0.88 and 0.87, respectively. The other fluid properties will be found in the Appendix. 
+A fouling factor of 0.001 should be provided for each stream, and the allowable pressure drop on each stream is 10.0 psi.
+A number of 20-ft hairpins of 2- by lJ4-in. IPS pipe are available. How many hairpins are required?
+
+"""
+"""
+Example 2
+Double Kpe Dube Oil-Crude Oil Exchanger. 6,900 lb /hr of a 26°API lube oil must be cooled from 450 to. 350°F by 72,500 lb/hr of 34°API mid-conti¬ nent crude oil. The crude oil .will be heated from 300 to 310°F.
+A fouling factor of 0.003 should be provided for each stream, and the allowable pressure drop on each stream will be 10 psi.
+A number of 20-ft hairpins of 3- by 2-in. IPS pipe are available. How many must be used, and how shall they be arranged? 
+The viscosity of the crude oil may be obtained from Kg. 14. For the lube oil, viscosities are 1.4 centipoises at 500°F, 3.0 at 400°F, and 7.7 at 300°F. These viscosities are great enough to introduce an error if = 1 is assumed.
+
+"""
+
+"""
+Example 3
+Calculation of a Kerosene-Crude Oil Exchanger. 43,800 lb/hr of a 42°API kerosene leaves the bottom of a distilling column at 390°F and will be cooled to 200°F by 149,000 lb/hr of 34°API
+Mid-continent crude coming from storage at 100°F and heated to 170°F. A 10 psi pressure drop is permissible on both streams, and in accordance with Table 12, a combined dirt factor of 0.003 should be provided.
+Available for this service is a 21 3<t in. ID exchanger having 158 1 in. OD, 13 BWG tubes 16'0" long and laid out on 134-in. square pitch. The bundle is arranged for four passes, and baffles are spaced 5 in. apart.
+Will the exchanger be suitable; i.e., what is the dirt factor?
+"""
+
+"""
+Example 4
+Calculation of a Distilled-water-Raw-water Exchanger. 175,000 lb/hr of distilled water enters an exchanger at 93°F and leaves at 85°F. 
+The heat will be transferred to 280,000 lb/hr of raw water coming from supply at 75°F and leaving the exchanger at 80°F. 
+A 10 psi pressure drop may be expended on both streams while providing a fouling factor of 0.0005 for distilled water and 0.0015 for raw water when the tube velocity exceeds 6 fps.
+Available for this service is a 15)4 in. ID exchanger having-160 % in. OD, 18 BWG tubes 16'0" long and laid out on ij-fg-in. triangular pitch. 
+The bundle is arranged for two passes, and baffles are spaced 12 in. apart.
+Will the exchanger be suitable
+"""
+
+"""
+Example 5
+
+Calculation of a Phosphate Solution Cooler. 20,160 lb /hr of a 30% KjPO, solution, specific gravity at 120°F = 1.30, is to be cooled from 150 to 90°F using well water from 68 to 90°F. 
+Pressure drops of 10 psi are allowable on both streams, and a total dirt factor of 0.002 is required.
+Available for this service is a 10.02 in. ID 1-2 exchanger having 52 % in. OD, 16 BWG tubes 16'0" long laid out on 1-in. square pitch. 
+The bundle is arranged for two passes, and the baffles are spaced 2 in. apart.
+■Will the exchanger be suitable?
+"""
