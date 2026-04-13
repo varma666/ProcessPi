d0b4373b9b0514738cca76fb0c7257699e89418c
Raviteja Varma <nravitejavarma@gmail.com>
2026-04-13 12:30:34 +0530

    Convert U value to W/m2K in double_pipe.py

--- processpi/equipment/heatexchangers/double_pipe.py
+++ processpi/equipment/heatexchangers/double_pipe.py
@@ -13,7 +13,7 @@ class DoublePipeHX(HeatExchanger):
         th_out = self.hot_out.temperature.to("K").value if self.hot_out and self.hot_out.temperature else hot["t_k"] - q / (hot["m_dot"] * hot["cp"])
         tc_out = self.cold_out.temperature.to("K").value if self.cold_out and self.cold_out.temperature else cold["t_k"] + q / (cold["m_dot"] * cold["cp"])
         lmtd = self.lmtd(hot["t_k"], th_out, cold["t_k"], tc_out)
-        u = float(self.specs.get("U", 350.0))
+        u = float(self.specs.get("U", 350.0).to("W/m2K").value)
         area = float(self.specs.get("area", self.area(q, u, lmtd)))
         return {
             "hx_type": "double_pipe",
