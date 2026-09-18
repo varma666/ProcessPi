758e5842f2a5de95f8b2627ebfdc80d90de7d3f2
Raviteja Varma <nravitejavarma@gmail.com>
2026-09-18 16:29:28 +0530

    Add Acetonitrile component with properties and constants
    This class includes various physical and thermodynamic properties essential for process engineering calculations involving Acetonitrile.

---
+++ processpi/components/acetonitrile.py
@@ -0,0 +1,52 @@
+from .base import Component
+from processpi.units import *
+
+class Acetonitrile(Component):
+    """
+    Represents the properties and constants for Acetonitrile(C2H3N).
+
+    This class provides a comprehensive set of physical and thermodynamic properties
+    for Acetonitrile, which are essential for various process engineering calculations.
+    These properties are stored as class attributes and are available for use by other
+    calculation modules within the ProcessPI library.
+
+    **Properties:**
+    - `name`: The common name of the compound.
+    - `formula`: The chemical formula.
+    - `molecular_weight`: The molar mass in g/mol.
+    - `_critical_temperature`: The critical temperature, above which a substance
+      cannot exist as a liquid, regardless of pressure.
+    - `_critical_pressure`: The critical pressure, the vapor pressure at the
+      critical temperature.
+    - `_critical_volume`: The critical volume per kmole.
+    - `_critical_zc`: The critical compressibility factor.
+    - `_critical_acentric_factor`: The acentric factor, a measure of the
+      non-sphericity of the molecule.
+    - `_density_constants`: Constants for calculating density as a function of temperature.
+    - `_specific_heat_constants`: Constants for calculating specific heat capacity as a
+      function of temperature.
+    - `_viscosity_constants`: Constants for calculating viscosity as a function of
+      temperature.
+    - `_thermal_conductivity_constants`: Constants for calculating thermal conductivity
+      as a function of temperature.
+    - `_vapor_pressure_constants`: Constants for calculating vapor pressure as a
+      function of temperature using the Antoine equation or similar models.
+    - `_enthalpy_constants`: Constants for calculating enthalpy as a function of temperature.
+    """
+    name = "Acetonitrile"
+    formula = "C2H3N"
+    molecular_weight = 41.052
+
+    # Critical properties
+    _critical_temperature = Temperature(545.5, "K")
+    _critical_pressure = Pressure(4.83, "MPa")
+    _critical_volume = Volume(0.173, "m3")
+    _critical_zc = 0.184
+    _critical_acentric_factor = 0.3379
+
+    _density_constants = [1.3064, 0.22597, 545.5, 0.28678]
+    _specific_heat_constants = [97582,-122.2,0.34085,0,0]
+    _viscosity_constants = [-10.906, 872.02,0,0,0]
+    _thermal_conductivity_constants = [0.33192, -0.00043243,0,0,0]
+    _vapor_pressure_constants = [58.302,-5385.60,-5.4954, 5.36e-06, 2.0]
+    _enthalpy_constants = [4.3511, 0.34765,0,0,0]
